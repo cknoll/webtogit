cb4eb3a9abfe64b5782d67b03ddc93e0607b4b30
Carsten Knoll <carsten.knoll@posteo.de>
2021-11-27 16:02:38 +0100

    improve test_data separation

--- tests/test_core.py
+++ tests/test_core.py
@@ -21,45 +21,46 @@ class TestCore(unittest.TestCase):
 
     def setUp(self):
         self._set_workdir_to_project_root()
+        self.c = Core(testmode=True)
 
     def tearDown(self) -> None:
         self._set_workdir_to_project_root()
         
-        c = Core()
-        c.purge_pad_repo(ignore_errors=True)
-        
+        self.c.purge_pad_repo(ignore_errors=True)
+        del self.c
 
     def test_core1(self):
-        c = Core()
-        self.assertFalse(os.path.exists(c.repo_dir))
-        c.init_pad_repo()
-        self.assertTrue(os.path.isdir(c.repo_dir))
-        self.assertTrue(os.path.isfile(c.checkfile))
-        self.assertTrue(os.path.isdir(os.path.join(c.repo_dir, ".git")))
 
-        self.assertRaises(FileExistsError, c.init_pad_repo)
+        self.assertEqual(self.c.repo_name, "padstogit-test-repo")
+        self.assertFalse(os.path.exists(self.c.repo_dir))
+        self.c.init_pad_repo()
+        self.assertTrue(os.path.isdir(self.c.repo_dir))
+        self.assertTrue(os.path.isfile(self.c.checkfile))
+        self.assertTrue(os.path.isdir(os.path.join(self.c.repo_dir, ".git")))
 
-        c.purge_pad_repo()
-        self.assertFalse(os.path.exists(c.repo_dir))
+        self.assertRaises(FileExistsError, self.c.init_pad_repo)
 
-        self.assertRaises(FileNotFoundError, c.purge_pad_repo)
+        self.c.purge_pad_repo()
+        self.assertFalse(os.path.exists(self.c.repo_dir))
+
+        self.assertRaises(FileNotFoundError, self.c.purge_pad_repo)
 
         # ensure that the directory is only deleted if the special file is present
-        c.init_pad_repo()
-        os.rename(c.checkfile, f"{c.checkfile}_backup")
-        self.assertRaises(FileNotFoundError, c.purge_pad_repo)
-        self.assertTrue(os.path.isdir(c.repo_dir))
+        self.c.init_pad_repo()
+        os.rename(self.c.checkfile, f"{self.c.checkfile}_backup")
+        self.assertRaises(FileNotFoundError, self.c.purge_pad_repo)
+        self.assertTrue(os.path.isdir(self.c.repo_dir))
 
         # now delete the repo
-        os.rename(f"{c.checkfile}_backup", c.checkfile)
-        c.purge_pad_repo()
-        self.assertFalse(os.path.exists(c.repo_dir))
+        os.rename(f"{self.c.checkfile}_backup", self.c.checkfile)
+        self.c.purge_pad_repo()
+        self.assertFalse(os.path.exists(self.c.repo_dir))
 
     def test_load_sources(self):
 
-        c = Core()
-        c.init_pad_repo()
-        sources = c.load_pad_sources(os.path.join(TEST_DATA_DIR, "sources.yml"))
+        self.c = self.c
+        self.c.init_pad_repo()
+        sources = self.c.load_pad_sources(os.path.join(TEST_DATA_DIR, "sources.yml"))
 
         self.assertEqual(len(sources), 3)
         
@@ -68,6 +69,15 @@ class TestCore(unittest.TestCase):
         
         self.assertEqual(sources[1]["url"], "https://etherpad.wikimedia.org/p/padstogit_testpad2")
         self.assertEqual(sources[1]["name"], "renamed_testpad.md"        )
+
+
+    def test_download(self):
+        
+        self.c.init_pad_repo()
+        
+
+
+
         
 
         
