982dc806806fdfe9841494d0919c3970d61ebab4
Carsten Knoll <carsten.knoll@posteo.de>
2021-11-27 12:58:28 +0100

    add functionality for repo creation and deletion

--- README.md
+++ README.md
@@ -4,6 +4,36 @@
 
 PadsToGit is a simple command line tool to facilitate archiving of web based pads (colaboratively edited texts).
 
+## How it works
+
+There are the following files:
+
+- settings.yml
+- sources.yml
+Example:
+    ```yaml
+        # This is a comment and will be ignored. Same for empty lines.
+
+        # The top YAML-element is a list. Entries are strings or dicts.
+        # The following two list entries are just simple literal strings
+        # (one url per line):
+
+        - https://pad.url1.org/p/some-pad
+        - https://pad.url1.org/p/some-other-pad
+
+        # The next url needs some additional information.
+        # It is thus stored as yaml-dictionary.
+
+        - https://pad.url1.org/p/some-third-pad:
+            name: explicit_filename.txt
+            key2: value2
+
+        - https://pad.url2.org/p/yet-another-pad
+    ```
+
+During installation a new git repository is created. The script parses `sources.yml` and downloads the content into the working dir of the repo and adds the file to the index. Then if there are changes, it makes a commit to the repo.
+
+
 ## Usage
 
 - Rename directory `src/package_name`
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,13 +1,48 @@
 import unittest
-from ipydex import IPS, activate_ips_on_exception
+import os
+from padstogit import Core
 
-activate_ips_on_exception()
+from ipydex import IPS, activate_ips_on_exception
+# activate_ips_on_exception()
 
 # noinspection PyPep8Naming
 class TestCore(unittest.TestCase):
     def setUp(self):
         pass
 
-    # mark tests which only work for the "old core"
+    def tearDown(self) -> None:
+
+        try:
+            cwd = os.getcwd()
+        except FileNotFoundError:
+            # this happens before any test is run. Seems to be a quirk of unittest.
+            return
+        c = Core()
+        c.purge_pad_repo(ignore_errors=True)
+        
+
     def test_core1(self):
-        pass
+        c = Core()
+        self.assertFalse(os.path.exists(c.repo_dir))
+        c.init_pad_repo()
+        self.assertTrue(os.path.isdir(c.repo_dir))
+        self.assertTrue(os.path.isfile(c.checkfile))
+        self.assertTrue(os.path.isdir(os.path.join(c.repo_dir, ".git")))
+
+        self.assertRaises(FileExistsError, c.init_pad_repo)
+
+        c.purge_pad_repo()
+        self.assertFalse(os.path.exists(c.repo_dir))
+
+        self.assertRaises(FileNotFoundError, c.purge_pad_repo)
+
+        # ensure that the directory is only deleted if the special file is present
+        c.init_pad_repo()
+        os.rename(c.checkfile, f"{c.checkfile}_backup")
+        self.assertRaises(FileNotFoundError, c.purge_pad_repo)
+        self.assertTrue(os.path.isdir(c.repo_dir))
+
+        # now delete the repo
+        os.rename(f"{c.checkfile}_backup", c.checkfile)
+        c.purge_pad_repo()
+        self.assertFalse(os.path.exists(c.repo_dir))
